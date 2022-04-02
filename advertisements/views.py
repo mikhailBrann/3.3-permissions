d76b40b52f3d7977c9733e55faffdaf716a5dc98
mikhailBrann <alunos13@yandex.ru>
2022-04-02 19:05:45 +0300

    initial commit

---
+++ advertisements/views.py
@@ -0,0 +1,25 @@
+from django_filters.rest_framework import DjangoFilterBackend
+from rest_framework.filters import SearchFilter
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.viewsets import ModelViewSet
+
+from advertisements.models import Advertisement
+from advertisements.permissions import IsOwnerOrReadOnlyPermission
+from advertisements.serializers import AdvertisementSerializer
+
+
+class AdvertisementViewSet(ModelViewSet):
+    """ViewSet для объявлений."""
+
+    # TODO: настройте ViewSet, укажите атрибуты для кверисета,
+    #  сериализаторов и фильтров
+    queryset = Advertisement.objects.all()
+    serializer_class = AdvertisementSerializer
+    filter_backends = [DjangoFilterBackend, SearchFilter]
+    filterset_fields = ['creator']
+
+    def get_permissions(self):
+        """Получение прав для действий."""
+        if self.action in ["create", "update", "partial_update"]:
+            return [IsAuthenticated(), IsOwnerOrReadOnlyPermission()]
+        return []
